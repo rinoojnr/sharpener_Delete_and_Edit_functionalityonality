457c26dcb6006a115c4cf317a9b68b638e43bc60
rinoojnr <rinoojnr@gmail.com>
2023-10-07 15:36:29 +0530

    Added Description and filter for descrption

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var form = document.getElementById("addForm");
 var itemList =document.getElementById("items");
 var filter =document.getElementById('filter');
 
+
 //Form submit event
 form.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeLi);
@@ -11,18 +12,25 @@ function addItem(e){
     e.preventDefault();
     //get input value
     var text = document.getElementById('item')
+    var des =document.getElementById('des')
   
     //create element
     var li =document.createElement('li');
     li.className='list-group-item';
     //add text node
     li.appendChild(document.createTextNode(text.value))
-    
+    li.appendChild(document.createTextNode(" "))
+    li.appendChild(document.createTextNode(des.value))
     //create button
     var buttton =document.createElement('button')
+   
     buttton.className="btn btn-danger btn-sm float-right delete";
     buttton.appendChild(document.createTextNode('X'))
     li.appendChild(buttton)
+    var edit =document.createElement('button')
+    edit.className="btn btn-warning btn-sm float-right"
+    edit.appendChild(document.createTextNode('Edit'))
+    li.appendChild(edit)
     itemList.appendChild(li)
 }
 function removeLi(e){
@@ -39,7 +47,12 @@ function FilterList(e){
     var itemList =document.getElementsByTagName('li')
     Array.from(itemList).forEach(function(item){
         var itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text)!=-1){
+        if(item.childNodes[2].textContent!='Edit'){
+            var itemDes =item.childNodes[2].textContent;
+        }else{
+            var itemDes ="";
+        }
+        if(itemName.toLowerCase().indexOf(text)!=-1 || itemDes.toLowerCase().indexOf(text)!=-1){
             item.style.display='block'
         }else{
             item.style.display='none'
